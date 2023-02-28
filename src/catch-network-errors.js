8ef2bef7805697c5ca94c645374d1d54d2580e46
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2023-02-28 13:39:55 +0000

    Remove n-logger
    This log is pretty useless, it doesn't tell us anything, and n-logger
    being a dependency of this library increases the complexity of the
    logging migration that we want to do.
    
    See discussion in Slack here:
    https://financialtimes.slack.com/archives/C04B0SADVNJ/p1677591422848369

--- src/catch-network-errors.js
+++ src/catch-network-errors.js
@@ -1,10 +1,7 @@
 'use strict';
 
-const logger = require('@financial-times/n-logger').default;
-
 module.exports = function (err) {
 	if (err.message.indexOf('network timeout at') > -1) {
-		logger.warn('Fetch network error occurred', { message: err.message });
 		return { ok: false };
 	} else {
 		throw err;
